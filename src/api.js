74dbfad134286943180744f5015c3ab2988f9f14
shagam <eli.shagam@gmail.com>
2023-06-20 14:55:06 +0300

    modified:   src/api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const serverless = require ('serverless-http');
-
-
+const serverless = require("serverless-http");
+const SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
 const app = express();
-
-
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 
-let records = []
+router.get("/search", (req, res) => {
+    // function that searches for a song with the Spotify API
+});
 
-// get all students
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
 
 router.get ('/', (req, res) => {
     res.json ({'hello': 'hi!'})
